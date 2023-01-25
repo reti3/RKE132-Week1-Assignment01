46913989433f2cda0cce06d40425dcc5f8905bca
Reti Kalde <reti@Retis-MacBook-Air.local>
2023-01-25 22:15:06 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// rakendus küsib kasutaja nime; programmis ei saa ole sama nimega muutujaid
+//käsuga on alati ümmarguste sulgude vahel, programm peab midagi tegema
+Console.WriteLine("Sisesta oma nimi:"); //output 
+string userName = Console.ReadLine(); //input = sisend, mida sisestame konsooli
+
+//string interpolation; peab lisama dollari märgi
+Console.WriteLine($"Hello, {userName}!");
+
